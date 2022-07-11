40b2545bc2875c54f8c4ab8b9d116bc8574639e1
indodesigner <webdesigner@indocosmo.com>
2022-07-11 17:42:47 +0530

    page edit

--- js/script.js
+++ js/script.js
@@ -328,24 +328,20 @@
 
 
 
-    // ...............floating back to top button......................
-
-    //Get the button
-var mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+// .....................floating btn.........................................
+$(document).ready(function() {
+  $('.go-top').hide(0)
+  
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 100){
+      $('.go-top').fadeIn(200);      
+    } else {
+      $('.go-top').fadeOut(300);
+    }
+  });
+  $('.go-top').click(function() {
+    event.preventDefault();
+    
+    $('html , body').animate({scrollTop: 0}, 1000);
+  });
+});
